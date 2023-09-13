1033f5a5579decbec00d7c57fc12d9265ba45176
Josh Danielle Domingo <domingo.237383@marikina.sti.edu.ph>
2023-09-13 21:28:23 +0800

    wew

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.10/firebase-app.js"
-import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.10/firebasedatabase.js"
-console.log(add(1,1))
+import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {add} from "./functions.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://capstone-370b9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  databaseURL: "https://capstone-370b9-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
+console.log (add(2,3))
 const app = initializeApp(appSettings)
-const database = getDatabase(app) 
-const pizzadb = ref(database, "Pizza")
-console.log(app)
+const database = getDatabase(app)
+
+const pizzaDB = ref (database,"Pizza")
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click",function(){
+let inputValue = inputFieldEl.value
+ console.log('${inputValue} added to database')
+ push(pizzaDB, inputValue)
+})
