ae7d6df8a7b3edd96a06dc3caa4584a75936fb75
Josh Danielle Domingo <domingo.237383@marikina.sti.edu.ph>
2023-10-13 21:09:26 +0800

    wwe

---
+++ Menupage/Menu.js
@@ -0,0 +1,25 @@
+import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+  databaseURL: "https://capstone-370b9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+
+const pizzaDB = ref (database,"Pizza")
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const cartListEl = document.getElementById("cart-item")
+
+let input = document.querySelector('input');
+input.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+      let inputValue = inputFieldEl.value
+      console.log(`${inputValue} added to database`)
+      push(pizzaDB, inputValue)
+      inputFieldEl.value=""
+      cartListEl.innerHTML += `<li>${inputValue}</li>`
+    }
+})
