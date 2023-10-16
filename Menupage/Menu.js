41ea2548dc9c022765e6bcab2491a677afa9c05b
Josh Danielle Domingo <domingo.237383@marikina.sti.edu.ph>
2023-10-16 21:41:34 +0800

    wew

--- Menupage/Menu.js
+++ Menupage/Menu.js
@@ -1,25 +1,90 @@
-import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const searchInput = document.getElementById("search");
+const menuItems = document.querySelectorAll(".menu-item");
+const cartContainer = document.getElementById("cart-container");
+const cartItemsList = document.getElementById("cart-items");
 
-const appSettings = {
-  databaseURL: "https://capstone-370b9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+menuItems.forEach((menuItem) => {
+    const addToCartButton = menuItem.querySelector(".add-to-cart");
+    addToCartButton.addEventListener("click", () => {
+        addToCart(menuItem);
+        alert("Added to Cart!");
+    });
+});
+
+searchInput.addEventListener("input", () => {
+    filterItems(searchInput.value.toLowerCase());
+});
+
+function addToCart(menuItem) {
+    const name = menuItem.getAttribute("data-name");
+    const price = parseFloat(menuItem.getAttribute("data-price"));
+    const size = menuItem.getAttribute("data-size");
+    
+    // Check if the item is already in the cart
+    const existingCartItem = cartItemsList.querySelector(`[data-name="${name}"]`);
+    if (existingCartItem) {
+        // If it exists, increment the quantity and update the price
+        const quantityElement = existingCartItem.querySelector(".quantity");
+        const quantity = parseInt(quantityElement.textContent);
+        const newQuantity = quantity + 1;
+        quantityElement.textContent = newQuantity;
+        const totalPriceElement = existingCartItem.querySelector(".total-price");
+        const total = price * newQuantity;
+        totalPriceElement.textContent = `$${total.toFixed(2)}`;
+    } else {
+        // If it's a new item, add it to the cart
+        const cartItem = document.createElement("li");
+        cartItem.classList.add("cart-item");
+        cartItem.setAttribute("data-name", name);
+        cartItem.innerHTML = `
+            <img src="${menuItem.querySelector('img').getAttribute('src')}" alt="${name}">
+            <span>${name}</span>
+            <span class="quantity">1</span>
+            <span class="total-price">$${price.toFixed(2)}</span>
+            <button class="remove-from-cart">Remove</button>
+        `;
+        cartItemsList.appendChild(cartItem);
+
+
+        const removeFromCartButton = cartItem.querySelector(".remove-from-cart");
+        removeFromCartButton.addEventListener("click", () => {
+            cartItemsList.removeChild(cartItem);
+        });
+    }
+
+    // Show the cart container
+    cartContainer.style.display = "block";
 }
 
-const app = initializeApp(appSettings)
-const database = getDatabase(app)
+function filterItems(query) {
+    menuItems.forEach((menuItem) => {
+        const name = menuItem.getAttribute("data-name").toLowerCase();
+        if (name.includes(query)) {
+            menuItem.style.display = "inline-block";
+        } else {
+            menuItem.style.display = "none";
+        }
+    });
+}
 
-const pizzaDB = ref (database,"Pizza")
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
-const cartListEl = document.getElementById("cart-item")
+$(document).ready(function(){
 
-let input = document.querySelector('input');
-input.addEventListener('keyup', (e) => {
-    if(e.keyCode === 13) {
-      let inputValue = inputFieldEl.value
-      console.log(`${inputValue} added to database`)
-      push(pizzaDB, inputValue)
-      inputFieldEl.value=""
-      cartListEl.innerHTML += `<li>${inputValue}</li>`
-    }
-})
+    $("a").on('click', function(event) {
+  
+
+      if (this.hash !== "") {
+
+        event.preventDefault();
+  
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
