91b5e866b4b65c16bae222fcd616e4a39817d6a0
Josh Danielle Domingo <domingo.237383@marikina.sti.edu.ph>
2023-11-13 11:41:59 +0800

    website

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import{initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-a
 import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-  databaseURL: "https://capstone-370b9-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  databaseURL: "https://maczelapizza-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
@@ -13,6 +13,7 @@ const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const cartListEl = document.getElementById("cart-item")
 
+// Pressing Enter key in Search bar
 let input = document.querySelector('input');
 input.addEventListener('keyup', (e) => {
     if(e.keyCode === 13) {
@@ -21,14 +22,19 @@ input.addEventListener('keyup', (e) => {
       push(pizzaDB, inputValue)
       inputFieldEl.value=""
       cartListEl.innerHTML += `<li>${inputValue}</li>`
+     
     }
-})
+     })
+    
 
 addButtonEl.addEventListener("click",function(){
 let inputValue = inputFieldEl.value
  console.log(`${inputValue} added to database`)
  push(pizzaDB, inputValue)
  inputFieldEl.value=""
- `cartListEl.innerHTML += <li>${inputValue}</li>`
+
+ cartListEl.innerHTML += `<li>${inputValue}</li>`
 })
+
+
 
